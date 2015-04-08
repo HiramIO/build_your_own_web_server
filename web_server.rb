3e8a183a78d3861b7a3ad3a64bbd9fed94ece5dd
Harrison Taylor <h.taylor@project89.org>
2015-04-07 23:20:35 -0400

    Experimented a lot, reverted to original

--- web_server.rb
+++ web_server.rb
@@ -1,42 +1,39 @@
 
-require 'socket'                                    # Require socket from Ruby Standard Library (stdlib)
-require "pry"
+require 'socket'
 host = 'localhost'
 port = 2000
 
-server = TCPServer.open(host, port)                 # Socket to listen to defined host and port
-puts "Server started on #{host}:#{port} ..."        # Output to stdout that server started
+server = TCPServer.open(host, port)
+puts "Server started on #{host}:#{port} ..."
 
-loop do                                             # Server runs forever
-  client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
-#GET / HTTP/1.1
+loop do
+  client = server.accept
   lines = []
-  while (line = client.gets) && !line.chomp.empty?  # Read the request and collect it until it's empty
+  while (line = client.gets) && !line.chomp.empty?
     lines << line.chomp
   end
-  puts lines                                        # Output the full request to stdout
+  puts lines
 
   filename = lines[0].gsub(/GET\ \//,'').gsub(/\ HTTP.*/,'')
 
-  # binding.pry
   if File.exists?(filename)
     response_body = File.read(filename)
     success_header = []
     success_header << "HTTP/1.1 200 OK"
-    success_header << "Content-Type: text/html" # should reflect the appropriate content type (HTML, CSS, text, etc)
-    success_header << "Content-Length: #{response_body.length}" # should be the actual size of the response body
+    success_header << "Content-Type: text/html"
+    success_header << "Content-Length: #{response_body.length}"
     success_header << "Connection: close"
     header = success_header.join("\r\n")
   else
     response_body = "File not found\n"
     not_found_header = []
     not_found_header << "HTTP/1.1 404 Not Found"
-    not_found_header << "Content-Type: text/plain" # is always text/plain
-    not_found_header << "Content-Length: #{response_body.length}" # should the actual size of the response body
+    not_found_header << "Content-Type: text/plain"
+    not_found_header << "Content-Length: #{response_body.length}"
     not_found_header << "Connection: close"
     header = not_found_header.join("\r\n")
   end
   response = [header, response_body].join("\r\n\r\n")
-  client.puts(response)                       # Output the current time to the client
-  client.close                                  # Disconnect from the client
+  client.puts(response)
+  client.close
 end
